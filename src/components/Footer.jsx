8ed1b1a02756dca542ac991ce6af0f216ab98e94
PankajKumar17 <pankajfriend17910@gmail.com>
2025-01-11 22:57:36 +0530

    Made footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,9 +18,13 @@ const Footer = () => {
             <div className="contact-details">
                 <div className="contact-card">
                     <img src="Assets/nabajit.svg" alt="" />
+                    <h2>Nabjit Das</h2>
+                    <p> +91_8509442084</p>
                 </div>
                 <div className="contact-card">
                     <img src="Assets/nisha.svg" alt="" />
+                    <h2>Nisha Dhaka</h2>
+                    <p>+91_7015797784</p>
                 </div>
             </div>
             </div>
@@ -32,10 +36,10 @@ const Footer = () => {
         <div className="social-media">
             <h3>Connect with us on:</h3>
             <div className="icons">
-                <img src="Assets/insta.svg" alt="" />
-                <img src="Assets/linkedin.svg" alt="" />
-                <img src="Assets/fb.svg" alt="" />
-                <img src="Assets/location.svg" alt="" />
+                <a href="https://www.instagram.com/aittechnicalboard?igsh=bGM3MTh4NTBnYWVq"><img src="Assets/insta.svg" alt="" /></a>
+                <a href="https://www.linkedin.com/company/ait-technical-board/"><img src="Assets/linkedin.svg" alt="" /></a>
+                <a href="https://www.instagram.com/aittechnicalboard?igsh=bGM3MTh4NTBnYWVq"><img src="Assets/fb.svg" alt="" /></a>
+                <a href="https://www.linkedin.com/company/ait-technical-board/"><img src="Assets/location.svg" alt="" /></a>
             </div>
         </div>
         <div className="footer-note">Made with love Technical Board.</div>
