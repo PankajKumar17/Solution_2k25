c7c92b102815a945c22f4833a70e31daf49dd1b9
PankajKumar17 <pankajfriend17910@gmail.com>
2025-01-19 15:25:13 +0530

    added title to landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -10,7 +10,8 @@ function LandingPage() {
       .fromTo("#box2", { opacity: 0, bottom: "-400px" }, { opacity: 1, bottom: "300px", duration: 2, ease: "power2.out" })
       .fromTo("#box3", { opacity: 0, x: "100%" }, { opacity: 1, x: "0%", duration: 2.5, ease: "power2.out" }, "-=1")
       .fromTo("#box4", { opacity: 0, x: "-100%" }, { opacity: 1, x: "0%", duration: 1.5, ease: "power2.out" }, "-=1")
-      .fromTo("#box5", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "-=1");
+      .fromTo("#box5", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "-=1")
+      .fromTo(".title", { opacity: 0 }, { opacity: 1, duration: 1.5 }, "-=1");
   }, []);
 
   return (
@@ -18,6 +19,9 @@ function LandingPage() {
       <div id="box1"><img src="Assets/Desktop - 12.svg" alt="Desktop" /></div>
       <div id="box2"><img src="Assets/Moon.svg" alt="Moon" /></div>
       <div id="box3"><img src="Assets/BackMountain.svg" alt="Back Mountain" /></div>
+      <div className="title">
+      <img className="solution" src="Assets/SOLUTION_2K25.svg" alt="" />
+      </div>
       <div id="box4"><img src="Assets/LeftMountain.svg" alt="Left Mountain" /></div>
       <div id="box5"><img src="Assets/SideTree.svg" alt="Side Tree" /></div>
     </div>
